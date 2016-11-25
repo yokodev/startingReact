8d0569687746a35909d09d250a7ae9c7a3293bf9
jamaya <yokodevmaster>
2016-11-25 16:11:13 -0600

    refactor on search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-class SearchBar extends React.Component{
+class SearchBar extends Component {
+  render() {
+    return <input onChange={event=> console.log(event.target.value) }/>;
+  }
   
 }
 
